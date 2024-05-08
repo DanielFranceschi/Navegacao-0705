24d6182bb30b68461dbacf12f5fe61871c6a785a
DanielFranceschi <danielhfmarques@gmail.com>
2024-05-08 00:28:24 -0300

    Mudei o Title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Titulo',
+      title: 'Começo',
       theme: ThemeData(
       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
